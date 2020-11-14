f1d164f8628ce70ca0bd63454c080d90925656f0
Jesus Arcas Carrera <jesusarcascarrera@gmail.com>
2020-11-14 21:15:58 +0100

    add function for get dicc and get label

--- datasets/dataset_load.py
+++ datasets/dataset_load.py
@@ -11,16 +11,18 @@ def load_images(folder, raw = False):
     for filename in os.listdir(folder):
         
         img = cv2.imread(os.path.join(folder,filename),1)
-        if not raw:
-            img = cv2.resize(img,(64,64))
-            
         if img is not None:
-            images.append(img)
+            if not raw:
+                img = cv2.resize(img,(64,64))
+                
+            if img is not None:
+                images.append(img)
 
 
     images = [img/255. for img in images]
 
     return np.array(images)
+
 
 
 def load_raw(path = './dataset/'):
@@ -30,6 +32,7 @@ def load_raw(path = './dataset/'):
     
     
     folders = os.listdir(path)
+    folders.sort()
     for folder in folders:
         
         imgs = load_images(os.path.join(path,folder),True)
@@ -56,18 +59,16 @@ def load_raw(path = './dataset/'):
     
     return (x,y)
 
-
-def load_data(test_size= 0.3, path = './dataset/', dc = False):
-    
-    if dc:
-        import zipfile
-        
+def load_data(test_size= 0.3, path = './dataset/'):
+   
         
     images = np.array([])
     labels = np.array([])
     label = 0
     
     folders = os.listdir(path)
+    folders.sort()
+
     for folder in folders:
         
         imgs = load_images(os.path.join(path,folder))
@@ -92,14 +93,23 @@ def load_data(test_size= 0.3, path = './dataset/', dc = False):
     x = np.array(x)
     y = pd.get_dummies(y)
     y = np.array(y)
-    
+
     size = x.shape[0]
     split_size =  size - int(size*test_size)
     
     X_train = x[:split_size]
     X_test = x[split_size:]
-    y_train = y[:split_size]
-    y_test = y[split_size:]
+    Y_train = y[:split_size]
+    Y_test = y[split_size:]
     
     
-    return (X_train, y_train), (X_test,y_test)
+    return (X_train, Y_train), (X_test,Y_test)
+
+def get_dicc():
+  dicc = {0:"with_mask", 1:"without_mask", 2:"wrong_mask"}
+  
+  return dicc
+  
+def get_label(value):
+  dicc = get_dicc()
+  return dicc[np.argmax(value)]
