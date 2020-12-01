0ed9e52357c8bfc948947858a4188ecd920523a8
Jesus Arcas Carrera <jesusarcascarrera@gmail.com>
2020-12-01 21:31:39 +0100

    update

--- datasets/dataset_load_v3.py
+++ datasets/dataset_load_v3.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 
 def load_images(folder):
     dim = (96,96)
-    dim2 = (80,80)
+    dim2 = (24,24)
     images = []
     
     files = os.listdir(folder)
@@ -21,7 +21,15 @@ def load_images(folder):
         if img is not None:
             img = cv2.resize(img,dim)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            img2 = cv2.resize(img,dim2)
+            img2 = cv2.resize(img2,dim)
+
+            images.append(img2)
+            # images.append(cv2.flip(img2,1))
+
             images.append(img)
+            # images.append(cv2.flip(img,1))
 
  
     # images = [img/255. for img in images]
@@ -56,16 +64,30 @@ def load_data(test_size= 0.3, path = './dataset/', random_state = False):
         labels = np.concatenate([labels,np.array([label]*imgs.shape[0])])
         label += 1
       
-   
-
-
-
-    x = np.array(images)
-    y = pd.get_dummies(labels)
+    data = list(zip(images,labels))
+        
+    if not random_state:
+      random_state = int(time.time())
+    
+    print("Barajando")    
+    random.seed(int(random_state))
+    random.shuffle(data)
+    random.shuffle(data)
+    x,y = zip(*data)
+    x = np.array(x)
+    y = pd.get_dummies(y)
     y = np.array(y)
 
-    X_train,X_test,Y_train,Y_test = train_test_split(x,y, test_size = test_size, random_state = random_state)
-
+    size = x.shape[0]
+    split_size =  size - int(size*test_size)
+   
+    print("Repartiendo") 
+    X_train = x[:split_size]
+    X_test = x[split_size:]
+    X_test = X_test/255.
+    Y_train = y[:split_size]
+    Y_test = y[split_size:]
+    
     print("Random State: {}".format(random_state))
     return (X_train, Y_train), (X_test,Y_test)
 
